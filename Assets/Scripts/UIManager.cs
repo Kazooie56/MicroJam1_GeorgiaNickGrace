5fc9a515c25d73487c5b116f359ca79936d0e844
Kazooie56 <w0465155@campus.nscc.ca>
2026-09-22 22:18:12 -0300

    Integrated functionality between scenes

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,11 +8,15 @@ public class UIManager : MonoBehaviour
     public GameObject gamePlayUI;
     public GameObject pauseScreenUI;
     public GameObject gameOverScreenUI;
+    public GameObject gameplayScreen;
+
+
    
-    void Start()
-    {
-        ShowMainMenu();
-    }
+    //// I'm toggling this off for testing, should be fine to keep off if we remember to uncomment this method after testing or the game begins on the MainMenu
+    //void Start()
+    //{
+    //    ShowMainMenu();
+    //}
 
     public void HideAllUI()
     {
@@ -21,35 +25,53 @@ public class UIManager : MonoBehaviour
         gamePlayUI.SetActive(false);
         pauseScreenUI.SetActive(false);
         gameOverScreenUI.SetActive(false);
+        gameplayScreen.SetActive(false);
     }
 
     public void ShowMainMenu()
     {
         HideAllUI();
         mainMenuUI.SetActive(true);
+
+        Time.timeScale = 0f;
     }
 
     public void ShowInstructions()
     {
         HideAllUI();
         instructionsScreenUI.SetActive(true);
+
+        Time.timeScale = 0f;
     }
 
     public void ShowGamePlayUI()
     {
         HideAllUI();
         gamePlayUI.SetActive(true);
+
+        Time.timeScale = 1f;
     }
 
     public void ShowPauseScreenUI()
     {
-        HideAllUI();
+        gamePlayUI.SetActive(false);
         pauseScreenUI.SetActive(true);
+
+        Time.timeScale = 0f;
     }
 
     public void ShowGameOverScreenUI()
     {
         HideAllUI();
         gameOverScreenUI.SetActive(true);
+
+        Time.timeScale = 0f;
+    }
+    public void ShowGameplay()
+    {
+        HideAllUI();
+        gamePlayUI.SetActive(true);
+
+        Time.timeScale = 1f;
     }
 }
